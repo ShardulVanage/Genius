0077b62c6e9cf0f09893144431177479ef70e10a
ShardulVanage <vsharadul@gmail.com>
2024-03-01 14:47:49 +0530

    Wavey bg

--- next13-ai-saas-master/app/(landing)/page.tsx
+++ next13-ai-saas-master/app/(landing)/page.tsx
@@ -1,13 +1,18 @@
 import { LandingNavbar } from "@/components/landing-navbar";
 import { LandingHero } from "@/components/landing-hero";
 import { LandingContent } from "@/components/landing-content";
+import { WavyBackground } from "@/components/ui/wavy-background";
+
 
 const LandingPage = () => {
   return ( 
-    <div className="h-full ">
+    <div className="h-screen ">
+      <WavyBackground className="h-screen "  >
       <LandingNavbar />
       <LandingHero />
+     
       <LandingContent />
+      </WavyBackground>
     </div>
    );
 }
